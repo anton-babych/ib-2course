f323b81846713b86c125c0452fa777110b2e8023
a babych <babichanton4@gmail.com>
2022-09-28 12:15:58 +0300

    lab 3(1,2)

---
+++ lab2/Program.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using lab1;
+
+namespace lab2
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            //string openText = File.ReadAllText("file.dat");
+            string openText = "stol214151";
+            Console.WriteLine("open text: " + openText);
+
+            string key = RandomNumbersGenerator.GenerateRandomString(openText.Length);
+            Console.WriteLine("key: " + key);
+
+            var cipherText = Encrypt(openText, key);
+            Console.WriteLine("cipher: " + cipherText);
+
+            var decodedText = Decrypt(cipherText, key);
+            Console.WriteLine("open text: " + decodedText);
+
+            var cipherPhraseText = Encrypt("aaMit21c", key);
+            Console.WriteLine("ciphermit21: " + cipherPhraseText);
+            TryBruteForce(@"?g	W2f	W9,FFTH8b7K7p$[KUee@W,^IW,\W9,FEEe G1", out string decodedPhraseText);
+            
+            //GetCombinations(2);
+        }
+
+        public static List<string> GetCombinations(string allSymbols, int length)
+        {
+            var combinations = Extension.GetCombinationsAndPerm(allSymbols, length);
+            List<string> st = new List<string>();
+
+            foreach (var comb in combinations)
+            {
+                Extension.GetPer(comb, out List<string> pers);
+                foreach (var per in pers)
+                {
+                    Console.WriteLine(per);
+                    st.Add(per);
+                }
+            }
+
+            return st;
+        }
+
+        public static string Encrypt(string openText, string key)
+        {
+            var length = openText.Length;
+
+            string cipherText = "";
+
+            for (int i = 0; i < length; i++)
+                cipherText += (char) (openText[i] ^ key[i]);
+
+            return cipherText;
+        }
+
+        public static string Decrypt(string cipherText, string key)
+        {
+            var length = key.Length;
+            string openText = "";
+
+            for (int i = 0; i < length; i++)
+                openText += (char) (cipherText[i] ^ key[i]);
+
+            return openText;
+        }
+
+        public static bool TryBruteForce(string cipherText, out string decodedText, string phrase = "Mit21",
+            int length = 7)
+        {
+            string symbols = RandomNumbersGenerator.Symbols;
+            char[] symbolsArray = symbols.ToCharArray();
+            
+            int phraseLength = phrase.Length;
+            int possibleTries = length - phraseLength + 1;
+
+            decodedText = "";
+            string tempKey = "";
+
+            List<string> extraCombinations = GetCombinations(RandomNumbersGenerator.Symbols,length - phraseLength);
+            
+            for (int blocksToShiftPhrase = 0; blocksToShiftPhrase < possibleTries; blocksToShiftPhrase++)
+            {
+                var withOnlyPhrase = GenerateWithOnlyPhrase(phrase, length, phraseLength, blocksToShiftPhrase);
+
+                for (int j = 0; j < length; j++)
+                {
+                    if (withOnlyPhrase[j] != '\0') continue;
+
+                    /*foreach (var combination in extraCombinations)
+                    {
+                        char[] tempArrayWithCombinations = new char[withOnlyPhrase.Length];
+                        Array.Copy(withOnlyPhrase, tempArrayWithCombinations, withOnlyPhrase.Length);
+
+                        tempArrayWithCombinations[j] = combination; 
+                        
+                        
+                        char[] m = new char[length];
+                        for (int i = 0; i < length; i++) 
+                            m[i] = (char) (tempArrayWithCombinations[i] ^ cipherText[i]);
+
+                       
+                        
+                        if (new string(tempArrayWithCombinations) == cipherText)
+                        {
+                            Console.WriteLine("---------done---------");
+                            return true;
+                        }
+                    }*/
+                }
+            }
+
+            Console.WriteLine("not found");
+            return false;
+        }
+
+        private static char[] GenerateWithOnlyPhrase(string phrase, int length, int phraseLength, int blocksToShiftPhrase)
+        {
+            char[] tempArrayWithPhrase = new char[length];
+            for (int i = 0; i < phraseLength; i++)
+            {
+                tempArrayWithPhrase[blocksToShiftPhrase + i] = phrase[i];
+            }
+
+            return tempArrayWithPhrase;
+        }
+    }
+}
---
+++ lab3/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Text;
+using lab1;
+using lab2;
+
+namespace lab3
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            /*byte[] computeHashMd5 = ComputeHashMd5("01234567");
+            string format = Convert.ToBase64String(computeHashMd5);
+            Console.WriteLine("6Ts6DTbarv+YS+RM8JbiNg==" == format);
+            //byte[] bytes = Encoding.Unicode.GetBytes(format);
+            Console.WriteLine(format);
+            Console.WriteLine(ComputeGuid(computeHashMd5));*/
+
+            var combs2 = Extension.GetCombinationsAndPerm("0123456789", 8, false);
+            
+            if(TryFindPasswordFromCombiantions(combs2, out var password)) 
+                Console.WriteLine($"Found: password is {password}");
+        }
+
+        private static bool TryFindPasswordFromCombiantions(List<string> combs, out string password)
+        {
+            Guid guidToFind = Guid.Parse("564c8da6-0440-88ec-d453-0bbad57c6036");
+            
+            password = "";
+            foreach (var comb in combs)
+            {
+                //int.TryParse(comb, out var num);
+                //var myGuid = ComputeGuid(ComputeHashMd5(BitConverter.GetBytes(num)));
+                
+                var myGuid = ComputeGuid(ComputeHashMd5(comb));
+                
+                Console.WriteLine($"{myGuid} {myGuid == guidToFind} {guidToFind}");
+                
+                if (myGuid != guidToFind) continue;
+                password = comb;
+                
+                return true;
+            }
+
+            Console.WriteLine("not found");
+            return false;
+        }
+
+        static byte[] ComputeHashMd5(byte[] dataForHash) => 
+            MD5.Create().ComputeHash(dataForHash);
+
+        static byte[] ComputeHashMd5(string str) => 
+            MD5.Create().ComputeHash(Encoding.Unicode.GetBytes(str));
+
+        static Guid ComputeGuid(byte[] hash) =>
+            new Guid(hash);
+        
+        public static byte[] ComputeHashSha256(byte[] toBeHashed) => 
+            SHA256.Create().ComputeHash(toBeHashed);
+        
+        public static byte[] ComputeHmacsha1(byte[] toBeHashed, byte[] key) => 
+            new HMACSHA1(key).ComputeHash(toBeHashed);
+        
+        
+    }
+}
