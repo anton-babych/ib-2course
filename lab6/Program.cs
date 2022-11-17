4e5c7f614426a8c551bf0b5d767411dbf898ab68
a babych <babichanton4@gmail.com>
2022-11-17 22:52:17 +0200

    other

---
+++ lab6/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Text;
+
+namespace lab7
+{
+    internal class Program
+    {
+        private const string ProgramPath = "C:/Users/Abob/RiderProjects/Ib-2kurs/lab6/";
+        
+        public static void Main(string[] args)
+        {
+            Task3();
+        }
+
+        private static void Task1()
+        {
+            byte[] messageBytes = Encoding.UTF8.GetBytes("test");
+            
+            RsaTest rsaTest = new RsaTest();
+            rsaTest.AssignNewKey();
+            
+            byte[] encryptedMessage = rsaTest.EncryptFromMemory(messageBytes);
+            byte[] decryptedMessage = rsaTest.DecryptFromMemory(encryptedMessage);
+
+            Console.WriteLine(Encoding.UTF8.GetString(decryptedMessage));
+        }
+        
+        private static void Task2()
+        {
+            byte[] messageBytes = Encoding.UTF8.GetBytes("test");
+            string keyPath = ProgramPath + "privatekey.xml";
+            
+            RsaTest rsaTest = new RsaTest();
+            rsaTest.AssignNewKey(keyPath);
+            
+            byte[] encryptedMessage = rsaTest.EncryptFromFile(keyPath, messageBytes);
+            byte[] decryptedMessage = rsaTest.DecryptFromMemory(encryptedMessage);
+
+            Console.WriteLine(Encoding.UTF8.GetString(decryptedMessage));
+        }
+
+        private static void Task3()
+        {
+            byte[] messageBytes = Encoding.UTF8.GetBytes("test");
+            string keyPath = ProgramPath + "babych.xml";
+            
+            RsaTest rsaTest = new RsaTest();
+            rsaTest.AssignNewKey(keyPath);
+
+            //byte[] encryptedMessage = rsaTest.EncryptFromFile(keyPath, messageBytes);
+            //byte[] decryptedMessage = rsaTest.DecryptFromContainer(encryptedMessage);
+
+            //Console.WriteLine(Encoding.UTF8.GetString(decryptedMessage));
+        }
+    }
+}
---
+++ lab7/Program.cs
@@ -0,0 +1,31 @@
+﻿
+using System;
+using System.Text;
+
+namespace lab7
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("cagqjng");
+            Task1();
+        }
+
+        private static void Task1()
+        {
+            var eds = new Eds();
+
+            string publicKeyPath = "./publicKey.xml";
+
+            string message = "testing.";
+            byte[] sign = eds.SignMessage(publicKeyPath, message);
+            
+            string anotherMessage = "i'm fake";
+            byte[] anotherSign = eds.SignMessage(publicKeyPath, anotherMessage);
+
+            bool signCheck = eds.Compare(publicKeyPath, sign, anotherSign);
+            Console.WriteLine("keys are " + (signCheck ? "same" : "not same"));
+        }
+    }
+}
